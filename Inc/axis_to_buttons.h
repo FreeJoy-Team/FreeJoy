8e8a254af380eee45c65d5ef28b35fc0a28b379d
Yury Vostrenkov <yuvostrenkov@gmail.com>
2020-01-06 20:06:26 +0300

    Added dead axis interval between buttons from axis

--- Inc/axis_to_buttons.h
+++ Inc/axis_to_buttons.h
@@ -15,7 +15,7 @@
 #include "analog.h"
 
 #define AXIS_FULLSCALE			4095
-
+#define SWICTH_DEAD_ZONE		15
 
 void AxesToButtonsProcess (buttons_state_t * button_state_buf, app_config_t * p_config, uint8_t * pos);
 
--- Src/axis_to_buttons.c
+++ Src/axis_to_buttons.c
@@ -13,70 +13,70 @@ buttons_state_t 	buttons[MAX_AXIS_NUM * 11];
 uint8_t GetPressedFromAxis (analog_data_t axis_data, uint8_t btn_num, axis_to_buttons_t axis_to_btn)
 {
 	if (axis_data >= (uint32_t) axis_to_btn.points[0] * AXIS_FULLSCALE / 100
-			&& axis_data <= (uint32_t) axis_to_btn.points[1] * AXIS_FULLSCALE / 100
+			&& axis_data <= (uint32_t) axis_to_btn.points[1] * AXIS_FULLSCALE / 100 - SWICTH_DEAD_ZONE
 			&& btn_num == 0)
 	{
 		return 1;
 	}
 	
-	if (axis_data > (uint32_t) axis_to_btn.points[1] * AXIS_FULLSCALE / 100
-			&& axis_data <= (uint32_t) axis_to_btn.points[2] * AXIS_FULLSCALE / 100
+	if (axis_data > (uint32_t) axis_to_btn.points[1] * AXIS_FULLSCALE / 100 
+			&& axis_data <= (uint32_t) axis_to_btn.points[2] * AXIS_FULLSCALE / 100 - SWICTH_DEAD_ZONE
 			&& btn_num == 1)
 	{
 		return 1;
 	}
 	
 	if (axis_data > (uint32_t) axis_to_btn.points[2] * AXIS_FULLSCALE / 100
-			&& axis_data <= (uint32_t) axis_to_btn.points[3] * AXIS_FULLSCALE / 100
+			&& axis_data <= (uint32_t) axis_to_btn.points[3] * AXIS_FULLSCALE / 100 - SWICTH_DEAD_ZONE
 			&& btn_num == 2)
 	{
 		return 1;
 	}
 	
 	if (axis_data > (uint32_t) axis_to_btn.points[3] * AXIS_FULLSCALE / 100
-			&& axis_data <= (uint32_t) axis_to_btn.points[4] * AXIS_FULLSCALE / 100
+			&& axis_data <= (uint32_t) axis_to_btn.points[4] * AXIS_FULLSCALE / 100 - SWICTH_DEAD_ZONE
 			&& btn_num == 3)
 	{
 		return 1;
 	}
 	
 	if (axis_data > (uint32_t) axis_to_btn.points[4] * AXIS_FULLSCALE / 100
-			&& axis_data <= (uint32_t) axis_to_btn.points[5] * AXIS_FULLSCALE / 100
+			&& axis_data <= (uint32_t) axis_to_btn.points[5] * AXIS_FULLSCALE / 100 - SWICTH_DEAD_ZONE
 			&& btn_num == 4)
 	{
 		return 1;
 	}
 	
 	if (axis_data > (uint32_t) axis_to_btn.points[5] * AXIS_FULLSCALE / 100
-			&& axis_data <= (uint32_t) axis_to_btn.points[6] * AXIS_FULLSCALE / 100
+			&& axis_data <= (uint32_t) axis_to_btn.points[6] * AXIS_FULLSCALE / 100 - SWICTH_DEAD_ZONE
 			&& btn_num == 5)
 	{
 		return 1;
 	}
 	
 	if (axis_data > (uint32_t) axis_to_btn.points[6] * AXIS_FULLSCALE / 100
-			&& axis_data <= (uint32_t) axis_to_btn.points[7] * AXIS_FULLSCALE / 100
+			&& axis_data <= (uint32_t) axis_to_btn.points[7] * AXIS_FULLSCALE / 100 - SWICTH_DEAD_ZONE
 			&& btn_num == 6)
 	{
 		return 1;
 	}
 	
 	if (axis_data > (uint32_t) axis_to_btn.points[7] * AXIS_FULLSCALE / 100
-			&& axis_data <= (uint32_t) axis_to_btn.points[8] * AXIS_FULLSCALE / 100
+			&& axis_data <= (uint32_t) axis_to_btn.points[8] * AXIS_FULLSCALE / 100 - SWICTH_DEAD_ZONE
 			&& btn_num == 7)
 	{
 		return 1;
 	}
 	
 	if (axis_data > (uint32_t) axis_to_btn.points[8] * AXIS_FULLSCALE / 100
-			&& axis_data <= (uint32_t) axis_to_btn.points[9] * AXIS_FULLSCALE / 100
+			&& axis_data <= (uint32_t) axis_to_btn.points[9] * AXIS_FULLSCALE / 100 - SWICTH_DEAD_ZONE
 			&& btn_num == 8)
 	{
 		return 1;
 	}
 	
 	if (axis_data > (uint32_t) axis_to_btn.points[9] * AXIS_FULLSCALE / 100
-			&& axis_data <= (uint32_t) axis_to_btn.points[10] * AXIS_FULLSCALE / 100
+			&& axis_data <= (uint32_t) axis_to_btn.points[10] * AXIS_FULLSCALE / 100 - SWICTH_DEAD_ZONE
 			&& btn_num == 9)
 	{
 		return 1;
@@ -243,7 +243,6 @@ void AxesToButtonsProcess (buttons_state_t * button_state_buf, app_config_t * p_
 					default:
 						break;
 				}
-				
 				(*pos)++;
 			}
 		}
